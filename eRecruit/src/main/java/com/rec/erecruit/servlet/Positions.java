d5cb4723d8fe414c265407ff434e290a000b031a
10Alecs <46989577+10Alecs@users.noreply.github.com>
2021-01-11 00:08:14 +0200

    session

--- eRecruit/src/main/java/com/rec/erecruit/servlet/Positions.java
+++ eRecruit/src/main/java/com/rec/erecruit/servlet/Positions.java
@@ -7,6 +7,7 @@ package com.rec.erecruit.servlet;
 
 import com.rec.erecruit.common.PositionDetails;
 import com.rec.erecruit.ejb.PositionBean;
+import com.rec.erecruit.ejb.UserBean;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
@@ -17,6 +18,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  *
@@ -27,6 +29,9 @@ public class Positions extends HttpServlet {
 
     @Inject
     private PositionBean positionBean;
+    
+    @Inject
+    private UserBean userBean;
 
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
@@ -83,20 +88,22 @@ public class Positions extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        
-            String[] PosIdsAsString = request.getParameterValues("pos_ids");
-            if (PosIdsAsString != null) {
-                List<Integer> positionIds = new ArrayList<>();
-                for (String positionIdAsString : PosIdsAsString) {
-                    positionIds.add(Integer.parseInt(positionIdAsString));
-                }
-                positionBean.deletePositionByIds(positionIds);
+
+        String[] PosIdsAsString = request.getParameterValues("pos_ids");
+        if (PosIdsAsString != null) {
+            List<Integer> positionIds = new ArrayList<>();
+            for (String positionIdAsString : PosIdsAsString) {
+                positionIds.add(Integer.parseInt(positionIdAsString));
             }
-            response.sendRedirect(request.getContextPath() + "/Positions");
-        String[] positionId = request.getParameterValues("apply");    
+            positionBean.deletePositionByIds(positionIds);
+        }
+        response.sendRedirect(request.getContextPath() + "/Positions");
+        String[] positionId = request.getParameterValues("apply");
         if (positionId != null) {
-            Integer userId=0;
-            Integer positionId_int=Integer.parseInt(request.getParameterValues("apply")[0]);
+            HttpSession session = request.getSession();
+            String usn = String.valueOf(session.getAttribute("uname"));
+            Integer userId = userBean.getIdByUsername(usn);
+            Integer positionId_int = Integer.parseInt(request.getParameterValues("apply")[0]);
             positionBean.createApplicant(userId, positionId_int);
         }
     }
