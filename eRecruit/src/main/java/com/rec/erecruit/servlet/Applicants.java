42af5e57f8a528cb2aa96dcd07a8b70202350fc3
Ratiu Marius-Andrei <mariusratiu@yahoo.com>
2021-01-12 14:54:22 +0200

    applicants

---
+++ eRecruit/src/main/java/com/rec/erecruit/servlet/Applicants.java
@@ -0,0 +1,105 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.rec.erecruit.servlet;
+
+import com.rec.erecruit.common.UserDetails;
+import com.rec.erecruit.ejb.ApplicantBean;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+import javax.inject.Inject;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author mariu
+ */
+@WebServlet(name = "Applicants", urlPatterns = {"/Applicants"})
+public class Applicants extends HttpServlet {
+
+    @Inject
+    private ApplicantBean applicantBean;
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet Applicants</title>");
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet Applicants at " + request.getContextPath() + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        request.setAttribute("activePage", "Applicants");
+
+        Integer pId = Integer.parseInt(request.getParameter("posIdForApplicants"));
+        System.out.println(pId);
+        List<UserDetails> users_applicants = applicantBean.applicantsToUsers(applicantBean.getAllApplicants(pId));
+        request.setAttribute("users_applicants", users_applicants);
+        request.getRequestDispatcher("/WEB-INF/pages/applicants.jsp").forward(request, response);
+        //response.sendRedirect(request.getContextPath() + "/Applicants");
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+     
+        
+        //processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
---
+++ eRecruit/src/main/webapp/WEB-INF/pages/applicants.jsp
@@ -0,0 +1,36 @@
+<%-- 
+    Document   : applicants
+    Created on : Jan 12, 2021, 10:41:52 AM
+    Author     : mariu
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<%@taglib prefix="t" tagdir="/WEB-INF/tags" %>
+<%@taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+
+<t:pageTemplate pageTitle="Applicants">
+    <form method="POST" action="${pageContext.request.contextPath}/Applicants">
+        <div class="d-flex justify-content-center" id="show_applicants">
+            <table class="table table-hover table-dark">
+                <thead>
+                    <tr>
+                        <th scope="col">Username</th>
+                        <th scope="col">Email</th>
+                        <th scope="col1" class="options_applicants">Options</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <c:forEach var="user" items="${users_applicants}" varStatus="Status">
+                        <tr>
+                            <td>${user.username}</td>
+                            <td>${user.email}</td>     
+                            <td class="options_applicants">            
+                                <button class="btn btn-primary" name="apply" value="" type="submit">Apply</button>
+                            </td>
+                        </tr>
+                    </c:forEach>
+                </tbody>
+            </table>
+    </form>
+</t:pageTemplate>
+
